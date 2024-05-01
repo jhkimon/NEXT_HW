001b951208a5e25378130dbdbf46dd60f513f0f7
jhkimon <sky06098i1@gmail.com>
2024-05-01 21:05:20 +0900

    refactor: 파일 수정

--- HW/NEXT_HW_12/blogProject/allauth_admin/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from allauth.socialaccount.models import SocialApp
-
-@admin.register(SocialApp)
-class SocialAppAdmin(admin.ModelAdmin):
-    list_display = ('provider', 'name', 'client_id')
-    search_fields = ('provider', 'name')
