70ff14c84858021b1be2f14fd4e69a7b2a2fdab3
jhkimon <sky06098i1@gmail.com>
2024-06-06 21:00:16 +0900

    session 22

---
+++ HW/NEXT_HW_22/weathersearch/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>© 2024 Weather App. All rights reserved.</div>
+        </footer>
+    );
+};
+
+export default Footer;
