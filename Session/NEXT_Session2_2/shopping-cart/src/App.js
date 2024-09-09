4551e2e2116149eb4126e4cc5089657ef0ae5711
jhkimon <sky06098i1@gmail.com>
2024-09-09 20:55:31 +0900

    session 2_2

---
+++ Session/NEXT_Session2_2/shopping-cart/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CartPage from './pages/CartPage';
+import ProductPage from './pages/ProductPage';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from 'store';
+import { store } from 'store';
+
+function App() {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/product/:id" element={<ProductPage />} />
+                        <Route path="/cart" element={<CartPage />} />
+                    </Routes>
+                </Router>
+            </PersistGate>
+        </Provider>
+    );
+}
+
+export default App;
