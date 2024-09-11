0dacd61bd34afe4bce4f6f8d4ab2b2cbc6c8d5ae
jhkimon <sky06098i1@gmail.com>
2024-09-11 21:43:56 +0900

    HW2 basic settings

---
+++ HW2/NEXT_HW_2/shoppingcart/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <h1>소개 페이지</h1>
+            <p>이 페이지는 Next.js로 만든 소개 페이지입니다.</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ HW2/NEXT_HW_2/shoppingcart/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>안녕하세요, Next.js!</h1>
+            <p>이것은 첫 번째 Next.js 페이지입니다.</p>
+        </div>
+    );
+}
